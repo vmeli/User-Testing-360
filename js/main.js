3b2b25c8bd7eefc5a2fbc970a09423b5aa29365b
vceli <vickensi.delacruz@gmail.com>
2016-06-08 06:25:30 -0500

    BOOTSTRAP

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+//
+
+$(document).ready(function(){
+ 
+  $(window).scroll(function(){
+    if( $(this).scrollTop() > 500 ){
+      $('.navegador').addClass('navegador2');
+    } else {
+      $('.navegador').removeClass('navegador2');
+    }
+  });
+ 
+});
+
